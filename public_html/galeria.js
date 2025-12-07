a90253fb2049ad65be65dfb2fcd7f352c9abda29
sergio <sergio@sergio-VirtualBox>
2025-12-07 20:33:23 +0100

    Versió 1.1.1: Afegir galeria d’imatges amb script

---
+++ public_html/galeria.js
@@ -0,0 +1,12 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
+ */
+
+// Codi de prova per engrandir la imatge en fer clic
+
+function ampliarImatge() {
+    // Aquí aniria el codi per ampliar la imatge
+    console.log("Ampliant imatge de la galeria...");
+}
+
