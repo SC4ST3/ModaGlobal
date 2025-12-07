c3b2e7ea6054a64a2d8b29f4435d666aec8c7fbe
sergio <sergio@sergio-VirtualBox>
2025-12-07 21:01:00 +0100

    Versió 1.1.2: Afegir formulari de contacte

---
+++ public_html/contacte.js
@@ -0,0 +1,12 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
+ */
+
+// contacte.js
+// Codi de prova per al formulari de contacte
+
+function enviarFormulari() {
+    console.log("Formulari enviat (simulació).");
+}
+
